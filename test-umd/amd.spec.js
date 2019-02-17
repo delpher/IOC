8bd8a02f53c12e9127d87339886697860ff499aa
delpher <a.onashyuk@gmail.com>
2019-02-17 19:41:23 +0200

    Added tests for UMD module format

---
+++ test-umd/amd.spec.js
@@ -0,0 +1,18 @@
+define(['src/container'], function(Container) {
+
+   describe('Container UMD module', function() {
+
+       it('should return Container', function() {
+          expect(Container).toBeDefined();
+          expect(Container.Singleton).toBeDefined();
+          expect(Container.DefaultLifetime).toBeDefined();
+
+          var container = new Container();
+          expect(typeof container.registerType).toBe('function');
+          expect(typeof container.registerInstance).toBe('function');
+          expect(typeof container.registerFactory).toBe('function');
+          expect(typeof container.resolve).toBe('function');
+       });
+
+   });
+});
---
+++ test-umd/commonjs.spec.js
@@ -0,0 +1,17 @@
+var Container = require('../src/container');
+
+describe('Container UMD module', function() {
+
+    it('should return Container', function() {
+        expect(Container).toBeDefined();
+        expect(Container.Singleton).toBeDefined();
+        expect(Container.DefaultLifetime).toBeDefined();
+
+        var container = new Container();
+        expect(typeof container.registerType).toBe('function');
+        expect(typeof container.registerInstance).toBe('function');
+        expect(typeof container.registerFactory).toBe('function');
+        expect(typeof container.resolve).toBe('function');
+    });
+
+});
