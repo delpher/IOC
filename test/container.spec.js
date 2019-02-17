42b9d8e9b37b9a59e250eb83cfe7c6e7bfed5867
delpher <a.onashyuk@gmail.com>
2019-02-17 14:55:33 +0200

    initial commit

---
+++ test/container.spec.js
@@ -0,0 +1,26 @@
+describe('Container', function() {
+
+    it('should be defined', function() {
+        expect(Container).toBeDefined();
+    });
+
+    // resolve instance from instance registration
+
+    // resolve instance from factory registration
+
+    // resolve instance from constructor registration
+
+    // resolve singleton from instance registration
+
+    // resolve singleton from factory registration
+
+    // resolve singleton from constructor registration
+
+    // resolve dependencies recursively
+
+    // resolve from child container
+
+    // satisfy imports no dependencies
+
+    // satisfy imports with dependencies
+});
